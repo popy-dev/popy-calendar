6c752ada87a775d2f1e836a2d2da1c2f5c832f67
Pierre Dudoret <pierre.dudoret@potogan.fr>
2018-01-31 22:01:34 +0100

    Initial commit : composer.json and interfaces

---
+++ src/FormaterInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Popy\Calendar;
+
+use DateTimeInterface;
+use Popy\Calendar\FormaterInterface;
+
+/**
+ * A Formater is responsible of formatting an input DateTimeInterface in a string
+ * representation, based on an input format.
+ *
+ * The format structure SHOULD be as similar as possible as the date() function format.
+ *
+ * The Formater MAY support some name localization.
+ */
+interface FormaterInterface
+{
+    /**
+     * Format a date into a string.
+     *
+     * @param DateTimeInterface $input  Input date.
+     * @param string            $format Date format
+     * 
+     * @return string
+     */
+    public function format(DateTimeInterface $input, $format);
+}
---
+++ src/ParserInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Popy\Calendar;
+
+use DateTimeInterface;
+
+/**
+ * A Parser is responsible to parse an input string, according to an input format,
+ * in order to build a DateTimeInterface matching the input representation as close
+ * as possible.
+ *
+ * If the Parser is related to a Formater, its format SHOULD be the same as the
+ * related Formater.
+ *
+ * The Parser MAY support some name localization.
+ */
+interface ParserInterface
+{
+    /**
+     * Parses a time string according to a specified format, then returns 
+     * DateTimeInterface or null if not able to match.
+     *
+     * @param string $input  Input date string.
+     * @param string $format Date format
+     * 
+     * @return DateTimeInterface|null
+     */
+    public function parse($input, $format);
+}
