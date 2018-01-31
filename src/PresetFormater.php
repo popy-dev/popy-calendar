76658541b81b231ad4d2742e90c67e7c976bff60
Pierre Dudoret <pierre.dudoret@potogan.fr>
2018-01-31 22:03:54 +0100

    Added utility classes

---
+++ src/PresetFormater.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Popy\Calendar;
+
+use DateTimeInterface;
+
+/**
+ * Utility/Helper class : Stores a preset date formatting to be used as a quick formater.
+ */
+class PresetFormater
+{
+    /**
+     * Formater.
+     *
+     * @var Formater
+     */
+    protected $formater;
+    
+    /**
+     * Preset format.
+     *
+     * @var string
+     */
+    protected $format;
+
+    /**
+     * Class constructor.
+     *
+     * @param FormaterInterface $Formater Formater
+     * @param string            $format   Date format
+     */
+    public function __construct(FormaterInterface $formater, $format)
+    {
+        $this->formater = $formater;
+        $this->format   = $format;
+    }
+
+    /**
+     * Format the input date.
+     *
+     * @param DateTimeInterface $input
+     * 
+     * @return string
+     */
+    public function format(DateTimeInterface $input)
+    {
+        return $this->formater->format($input, $this->format);
+    }
+}
---
+++ src/PresetParser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Popy\Calendar;
+
+use DateTimeInterface;
+
+/**
+ * Utility/Helper class : Stores a preset date formatting to be used as a quick parser.
+ */
+class PresetParser
+{
+    /**
+     * Parser.
+     *
+     * @var ParserInterface
+     */
+    protected $parser;
+    
+    /**
+     * Preset format.
+     *
+     * @var string
+     */
+    protected $format;
+
+    /**
+     * Class constructor.
+     *
+     * @param ParserInterface $parser parser
+     * @param string          $format   Date format
+     */
+    public function __construct(ParserInterface $parser, $format)
+    {
+        $this->parser = $parser;
+        $this->format = $format;
+    }
+
+    /**
+     * Parses the input string into a date.
+     *
+     * @param string $input
+     * 
+     * @return DateTimeInterface
+     */
+    public function format($input)
+    {
+        return $this->parser->parse($input, $this->format);
+    }
+}
