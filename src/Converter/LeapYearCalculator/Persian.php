2891315380d92633419e61fd6f07cfb817945b17
Pierre Dudoret <pierre.dudoret@potogan.fr>
2018-02-03 22:43:37 +0100

    [Converter] [LeapYearCalculator] Added Persian leap year calculator.

---
+++ src/Converter/LeapYearCalculator/Persian.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Popy\Calendar\Converter\LeapYearCalculator;
+
+use Popy\Calendar\Converter\LeapYearCalculatorInterface;
+
+/**
+ * Persian / Solar Hijri leap year calculator.
+ *
+ * @link https://en.wikipedia.org/wiki/Solar_Hijri_calendar#Solar_Hijri_algorithmic_calendar
+ */
+class Persian implements LeapYearCalculatorInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function isLeapYear($year)
+    {
+        $cycle = $year % 2820;
+
+        if ($cycle >= 21 * 128) {
+            $cycle -= 21 * 128;
+        } else {
+            $cycle = $cycle % 128;
+        }
+
+        if ($cycle >= 29) {
+            $cycle -= 29;
+            if ($cycle >= 2*33) {
+                $cycle -= 2 * 33
+            } else {
+                $cycle = $cycle % 33;
+            }
+        }
+
+        $cycle++;
+
+        return $cycle > 1 && ($cycle % 4 === 1);
+    }
+}
