4dbead80ac3e299c41c9b0b2393e33ee36a47965
Pierre Dudoret <pierre.dudoret@potogan.fr>
2018-02-08 12:40:47 +0100

    [Factory] Added GregorianCalendarFactory

---
+++ src/Factory/GregorianCalendarFactory.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Popy\Calendar\Factory;
+
+use Popy\Calendar\Calendar\ComposedCalendar;
+use Popy\Calendar\Converter\AgnosticConverter;
+use Popy\Calendar\Converter\UnixTimeConverter;
+use Popy\Calendar\Converter\LeapYearCalculator;
+use Popy\Calendar\Converter\DatePartsConverter;
+use Popy\Calendar\Converter\TimeConverter\DuoDecimalTime;
+use Popy\Calendar\Formater\Localisation;
+use Popy\Calendar\Formater\SymbolFormater;
+use Popy\Calendar\Formater\AgnosticFormater;
+use Popy\Calendar\Parser\AgnosticParser;
+use Popy\Calendar\Parser\ResultMapper;
+use Popy\Calendar\Parser\FormatLexer;
+use Popy\Calendar\Parser\FormatParser\PregExtendedNative;
+
+/**
+ * Helper factory to build a Gregogrian calendar.
+ */
+class GregorianCalendarFactory
+{
+    /**
+     * Builds a gregorian calendar.
+     *
+     * @return ComposedCalendar
+     */
+    public function build()
+    {
+        $calc = new LeapYearCalculator\Modern();
+        $locale = new Localisation\NativeHardcoded();
+
+        $converter = new AgnosticConverter(new UnixTimeConverter\Chain([
+            new UnixTimeConverter\GregorianDateFactory(),
+            new UnixTimeConverter\Date(),
+            new UnixTimeConverter\TimeOffset(),
+            new UnixTimeConverter\DateSolar($calc, 0, 1970),
+            new UnixTimeConverter\GregorianCalendarMonthes($calc),
+            new UnixTimeConverter\Iso8601Weeks($calc, 1970, 3),
+            new UnixTimeConverter\Time(),
+        ]));
+
+        $symbolFormater = new SymbolFormater\Chain([
+            new SymbolFormater\Litteral(),
+            new SymbolFormater\StandardDate(),
+            new SymbolFormater\StandardDateFragmented($locale),
+            new SymbolFormater\StandardDateSolar(),
+            new SymbolFormater\StandardDateTime(),
+            new SymbolFormater\StandardRecursive(),
+            new SymbolFormater\Litteral(true),
+        ]);
+
+        $formater = new AgnosticFormater(
+            new FormatLexer\MbString(),
+            $converter,
+            $symbolFormater
+        );
+
+        $mappers = new ResultMapper\Chain([
+            new ResultMapper\GregorianDateFactory(),
+            new ResultMapper\StandardDate(),
+            new ResultMapper\StandardDateFragmented(),
+            new ResultMapper\StandardDateSolar(),
+            new ResultMapper\StandardDateTime(),
+        ]);
+
+        $parser = new AgnosticParser(
+            new PregExtendedNative(),
+            $mappers,
+            $converter
+        );
+
+        return new ComposedCalendar($formater, $parser);
+    }
+}
