da0a069174c274777365c725311bf25107924faf
Pierre Dudoret <pierre.dudoret@potogan.fr>
2018-02-11 22:41:25 +0100

    [Formater] Added NumberConverter and improved 'y' formating & parsing

---
+++ src/Formater/NumberConverter/TwoDigitsYear.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Popy\Calendar\Formater\NumberConverter;
+
+use Popy\Calendar\Formater\NumberConverterInterface;
+
+/**
+ * Converts a number (typically a year) to a 2 digits representation, and back
+ * from it, using a referenceYear.
+ */
+class TwoDigitsYear implements NumberConverterInterface
+{
+    /**
+     * Reference century year.
+     *
+     * @var integer
+     */
+    protected $referenceYear;
+
+    /**
+     * @var boolean
+     */
+    protected $lateFiftyYearsArePreviousCentury;
+
+    /**
+     * Class constructor.
+     *
+     * @param integer $referenceYear                    Reference century year.
+     * @param boolean $lateFiftyYearsArePreviousCentury If true, input values greater than 50 will
+     *                                                  be resolved as previous century.
+     */
+    public function __construct($referenceYear = 2000, $lateFiftyYearsArePreviousCentury = true)
+    {
+        $this->referenceYear = $referenceYear - $referenceYear % 100;
+        $this->lateFiftyYearsArePreviousCentury = $lateFiftyYearsArePreviousCentury;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function to($input)
+    {
+        return sprintf('%02d', $input % 100);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function from($input)
+    {
+        $input = intval($input);
+
+        if ($this->lateFiftyYearsArePreviousCentury && $input > 50) {
+            return $this->referenceYear - 100 + $input;
+        }
+
+        return $this->referenceYear + $input;
+    }
+}
---
+++ src/Formater/NumberConverterInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Popy\Calendar\Formater;
+
+/**
+ * Number converters : convert an integer to/from another number format/system.
+ */
+interface NumberConverterInterface
+{
+    /**
+     * Converts an integer to implemented format/system.
+     *
+     * @param integer $input
+     *
+     * @return string
+     */
+    public function to($input);
+
+    /**
+     * Converts back a number from implemented format/system.
+     *
+     * @param string $input
+     *
+     * @return integer
+     */
+    public function from($input);
+}
